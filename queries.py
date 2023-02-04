3154532ce2783447a8b65a9e6e3047227969b12d
anekha sokhal <anekha17@yahoo.com>
2023-02-04 18:50:06 +0400

    Python & SQL 1

--- queries.py
+++ queries.py
@@ -5,7 +5,7 @@ def directors_count(db):
     query = "SELECT COUNT(name) FROM directors"
     db.execute(query)
     results = db.fetchall()
-    return results
+    return int(results[0][0])
 
 
 def directors_list(db):
@@ -27,13 +27,16 @@ def love_movies(db):
     query =  """SELECT title
             FROM movies
             WHERE UPPER(movies.title) LIKE 'LOVE %'
-            OR UPPER(movies.title) LIKE 'LOVE%'
-            OR UPPER(movies.title) LIKE '%LOVE%'
             OR UPPER(movies.title) LIKE '% LOVE'
             OR UPPER(movies.title) LIKE '% LOVE %'
-            OR UPPER(movies.title) LIKE '%LOVE%'
-            OR UPPER(movies.title) LIKE '%, LOVE'
-            OR UPPER(movies.title) LIKE 'LOVE, %'
+            OR UPPER(movies.title) LIKE '%, LOVE %'
+            OR UPPER(movies.title) LIKE '% LOVE, %'
+            OR UPPER(movies.title) LIKE '%, LOVE %'
+            OR UPPER(movies.title) LIKE '%. LOVE %'
+            OR UPPER(movies.title) LIKE '% LOVE. %'
+            OR UPPER(movies.title) LIKE '% LOVE. '
+            OR UPPER(movies.title) LIKE "%' LOVE %"
+            OR UPPER(movies.title) LIKE "% LOVE' %"
             ORDER BY title
             """
     db.execute(query)
@@ -44,25 +47,37 @@ def love_movies(db):
 def directors_named_like_count(db, name):
     # return the number of directors which contain a given word in their name
     #How many directors contain a word, given by a user, in their name?
-    query = """
-
-"""
-
-    c.execute(query, (f"%{name}%",)) # <- Will replace "?" in the query with %keyword%
-    rows = c.fetchall()
-    return rows
+    query = """SELECT count(name)
+            FROM directors
+            WHERE UPPER(name) LIKE ?
+            """
+    db.execute(query, (f"%{name}%",)) # <- Will replace "?" in the query with %keyword%
+    rows = db.fetchall()
+    return int(rows[0][0])
 
 
 def movies_longer_than(db, min_length):
     # return this list of all movies which are longer than a given duration,
     # sorted in the alphabetical order
-    pass  # YOUR CODE HERE
-
+    t = (min_length, )
+    query = """ SELECT title
+            FROM movies
+            WHERE minutes > ?
+            ORDER BY title
+            """
+    db.execute(query, t)
+    results = db.fetchall()
+    end_names = []
+    for i in results:
+        end_names.append(i[0])
+    return end_names
 
 import sqlite3
 
 conn = sqlite3.connect('data/movies.sqlite')
 db = conn.cursor()
-print(directors_count(db))
-print(directors_list(db))
-print(love_movies(db))
+#print(directors_count(db))
+#print(directors_list(db))
+#print(love_movies(db))
+print(directors_named_like_count(db, 'Lang'))
+#print(movies_longer_than(db, 300))
